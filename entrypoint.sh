ea6ad4379194e6119054ffb8b0c86e50a58175c4
Tonye Jack <jtonye@ymail.com>
2021-05-07 18:32:12 -0400

    Updated command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-FILES="${INPUT_FILES/\n/ }"
-
 # shellcheck disable=SC2086
 npm install -g $INPUT_PLUGINS
 
-# shellcheck disable=SC2086
-npx remark "$FILES" ${INPUT_ARGS}
-
+for path in ${INPUT_FILES}
+do
+  # shellcheck disable=SC2086
+  npx remark "$path" ${INPUT_ARGS}
+done
