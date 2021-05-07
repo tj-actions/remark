84d4fe7e19126b3c44e86b9e1fd41e4c02f0845b
Tonye Jack <jtonye@ymail.com>
2021-05-07 18:24:39 -0400

    Fixed test.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 FILES="${INPUT_FILES/\n/ }"
 
-npm install -g "$INPUT_PLUGINS"
+# shellcheck disable=SC2086
+npm install -g $INPUT_PLUGINS
 
 # shellcheck disable=SC2086
 npx remark "$FILES" ${INPUT_ARGS}
