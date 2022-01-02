2eb6ec6ac83d6571faf1031d9d59eb40ef05c92c
Tonye Jack <jtonye@ymail.com>
2022-01-01 21:05:38 -0500

    Add support for running on all platforms (#46)
    * Update action.yml
    
    * Update entrypoint.sh
    
    * Delete Dockerfile
    
    * Update action.yml
    
    * Update action.yml
    
    * Update entrypoint.sh
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * Update entrypoint.sh
    
    * Update test.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+npm install -g remark-cli
+
 # shellcheck disable=SC2086
 npm install -g $INPUT_PLUGINS
 
 for path in ${INPUT_FILES}
 do
   # shellcheck disable=SC2086
-  npx remark "$path" ${INPUT_ARGS}
+  npx remark --rc-path="${INPUT_CONFIG}" ${INPUT_ARGS} "$path"
 done
