aadb9157a2fec7febe3c0a1252aa9d18c35acc5e
Tonye Jack <jtonye@ymail.com>
2022-01-01 20:53:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,5 @@ npm install -g $INPUT_PLUGINS
 for path in ${INPUT_FILES}
 do
   # shellcheck disable=SC2086
-  npx remark "$path" ${INPUT_ARGS}
+  npx remark -r "${INPUT_CONFIG}" ${INPUT_ARGS} "$path"
 done
